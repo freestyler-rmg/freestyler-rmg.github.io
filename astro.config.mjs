0dc01652a2037a737337c26de58fe14fee4d3780
Roy Margasa <roy.margasa@gmail.com>
2025-07-24 17:18:05 +0700

    Fixed wrong base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://freestyler-rmg.github.io',
+  site: 'https://freestyler-rmg.github.io/',
+  base: '/',
 });
